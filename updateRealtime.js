cc6468edecf89244877f157848d2963a1e5cec04
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2020-12-03 21:24:53 +1100

    Update updateRealtime.js

--- updateRealtime.js
+++ updateRealtime.js
@@ -1,9 +1,9 @@
-﻿function showPreview(){
-  let html = document.getElementById("html").value;
-  let css = "<style>" + document.getElementById("css").value + "</style>";
-  let javascript = "<scri" + "pt>" + document.getElementById("javascript").value + "</scri" + "pt>";
-  let frame = document.getElementById("preview-window").contentWindow.document;
-  frame.open();
-  frame.write(html + css + javascript);
-  frame.close();
+function showPreview() {
+	let html = document.getElementById("html").value;
+	let css = "<style>" + document.getElementById("css").value + "</style>";
+	let javascript = "<scri" + "pt>" + document.getElementById("javascript").value + "</scri" + "pt>";
+	let frame = document.getElementById("preview-window").contentWindow.document;
+	frame.open();
+	frame.write(html + css + javascript);
+	frame.close();
 }
