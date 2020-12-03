2cde9ace13406f70615794a99c1e7fbd84c10a79
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2020-12-03 21:26:11 +1100

    Add files via upload

---
+++ buttonFunctions.js
@@ -0,0 +1,18 @@
+﻿function save() {
+	// Create file contents
+	let head = "<!doctype html><html><head><meta charset=\"utf-8\"><title>HTML</title></head>";
+	let html = document.getElementById("html").value;
+	let css = "<style>" + document.getElementById("css").value + "</style>";
+	let javascript = "<scri" + "pt>" + document.getElementById("javascript").value + "</scri" + "pt>";
+	let foot = "</body></html>";
+	let file =  head + css + "<body>" + html + javascript + foot;
+	
+	// Download file
+	let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(file));
+	element.setAttribute('download', "index.html");
+	element.style.display = 'none';
+	document.body.appendChild(element);
+	element.click();
+	document.body.removeChild(element);
+}
