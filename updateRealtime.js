aad1f87f227446dcdf142e43a5384c98f1dec3df
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2021-05-29 10:00:26 +1000

    Update updateRealtime.js

--- updateRealtime.js
+++ updateRealtime.js
@@ -5,6 +5,7 @@ function preview() {
 		let css = "<style>" + get("css").value + "</style>";
 		let javascript = "<script>" + get("javascript").value + "</script>";
 		let frame = get("preview-window").contentWindow.document;
+		frame.src = "about:blank";
 		frame.open();
 		frame.write(html + css + javascript);
 		frame.close();
